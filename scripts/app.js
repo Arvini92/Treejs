a4b57416b1f474272d17f3bd2dce83c009037ba9
Nicholas Sumshchenko <maybe@gmail.com>
2018-11-13 11:48:56 +0200

    working example

--- scripts/app.js
+++ scripts/app.js
@@ -2,11 +2,11 @@ var example = (function() {
 
     "use strict";
 
-    var scene = new THREE.Scene(),
-    renderer = window.WebGlRenderingContext ? new THREE.WebGLRenderer() : new THREE.CanvasRenderer(),
-    light = new THREE.AmbientLight(0xffffff),
-    camera,
-    box;
+    var scene = new THREE.Scene();
+    var renderer = new THREE.WebGLRenderer({ antialias: true });
+    var light = new THREE.AmbientLight(0xffffff);
+    var camera;
+    var box;
 
     function initScene() {
         renderer.setSize( window.innerWidth, window.innerHeight );
